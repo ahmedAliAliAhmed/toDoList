3caed2b428444d6417b138720fa7126bf19d2fa9
ahmedAliAliAhmed <theprofessore1416@gmail.com>
2018-07-23 02:02:25 +0800

    style.css

---
+++ script.js
@@ -0,0 +1,34 @@
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-countSpan')
+const uncheckedCountSpan = document.getElementById('unchecked-countSpan')
+
+
+ 
+
+ var count = 0
+// var uncheckedSpan = 0
+
+ function newTodo() {
+  const toDo =prompt(" " , "walk the dog")
+   // if(toDO != null) {
+	   
+	
+	   var li= document.createElement('li');
+	   list.appendChild(li);
+	  // li.setAttribute("id","li1")
+       li.innerHTML = " " + toDo + " "
+	   count++
+	//   uncheckedSpan++
+	   itemCountSpan.innerHTML =count;
+	 
+	   
+	 
+   }
+ 
